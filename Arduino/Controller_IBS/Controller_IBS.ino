10b2c83017cd5ef2cea8a50ea247ce1571d30893
ahmad <a7mad373737@gmail.com>
2019-07-29 17:39:35 +0200

    added joy support foor IBS

---
+++ Arduino/Controller_IBS/Controller_IBS.ino
@@ -0,0 +1,26 @@
+#include <ros.h>
+#include <sensor_msgs/Joy.h>
+
+ros::NodeHandle_<ArduinoHardware, 1, 1, 512, 128> nh;
+sensor_msgs::Joy msg;
+
+#include <HBridge.h>
+HBridge hbridge(3, 5, 10, 9);
+
+#include <joy_ps4_new.h>
+JoyControl joystick(hbridge);
+
+void messageCb( const sensor_msgs::Joy& msg) {
+  joystick.handle(msg);
+}
+
+ros::Subscriber<sensor_msgs::Joy> sub("joy", messageCb );
+
+void setup() {
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop() {
+  nh.spinOnce();
+}
---
+++ libs/joy_ps4_new/joy_ps4_new.h
@@ -0,0 +1,22 @@
+#ifndef _JOYPS4_H
+#define _JOYPS4_H
+
+#include <ros.h>
+#include <Arduino.h>
+#include <HBridge.h>
+#include <sensor_msgs/Joy.h>
+
+class JoyControl {
+private:
+   /*TODO handle analog values*/
+   /*int x = 0, y = 0;*/
+   uint8_t square, x, circle, tri, L1, R1, L2, R2, share, options, L3, R3, home, touchPad, x_, y_;
+   float R2_pwm, L2_pwm;
+   HBridge& hbridge;  /* Attached H-Bridge */
+
+public:
+  JoyControl(HBridge&);
+  void handle(sensor_msgs::Joy msg);
+};
+
+#endif
